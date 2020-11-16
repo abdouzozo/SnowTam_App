5dcc8afcf361df3025fa0e07a8f172aece31e496
Abdou <abdouzozo.14@gmail.com>
2020-11-16 09:39:46 +0100

    view init mainActivity

--- app/src/main/java/com/ensim/snowtam_app/controller/MainActivity.java
+++ app/src/main/java/com/ensim/snowtam_app/controller/MainActivity.java
@@ -3,14 +3,153 @@ package com.ensim.snowtam_app.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.*;
 
 import com.ensim.snowtam_app.R;
+import com.ensim.snowtam_app.model.InfoAeroport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText mChampsInput_1;
+    private EditText mChampsInput_2;
+    private EditText mChampsInput_3;
+    private EditText mChampsInput_4;
+    private Button mSearchButton;
+
+    private ArrayList<InfoAeroport> ListAeroports;
+
+    boolean EmptyChamps_1 = true;
+    boolean EmptyChamps_2 = true;
+    boolean EmptyChamps_3 = true;
+    boolean EmptyChamps_4 = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mChampsInput_1 = (EditText) findViewById(R.id.activity_main_input_1);
+        mChampsInput_2 = (EditText) findViewById(R.id.activity_main_input_2);
+        mChampsInput_3 = (EditText) findViewById(R.id.activity_main_input_3);
+        mChampsInput_4 = (EditText) findViewById(R.id.activity_main_input_4);
+
+
+        mSearchButton = (Button) findViewById(R.id.activity_main_search_btn);
+
+        mSearchButton.setEnabled(false);
+        mChampsInput_2.setEnabled(false);
+        mChampsInput_3.setEnabled(false);
+        mChampsInput_4.setEnabled(false);
+
+        ListAeroports = new ArrayList<InfoAeroport>();
+
+        mChampsInput_1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EmptyChamps_1 = !(s.toString().length() != 0);
+                mSearchButton.setEnabled(!EmptyChamps_1);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!EmptyChamps_1){
+                    mChampsInput_2.setEnabled(true);
+                }
+            }
+        });
+        mChampsInput_2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EmptyChamps_2 = !(s.toString().length() != 0);
+                mSearchButton.setEnabled(!EmptyChamps_1);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!EmptyChamps_2){
+                    mChampsInput_3.setEnabled(true);
+                }
+            }
+        });
+        mChampsInput_3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EmptyChamps_3 = !(s.toString().length() != 0);
+                mSearchButton.setEnabled(!EmptyChamps_1);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!EmptyChamps_3){
+                    mChampsInput_4.setEnabled(true);
+                }
+            }
+        });
+        mChampsInput_4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EmptyChamps_4 = !(s.toString().length() != 0);
+                mSearchButton.setEnabled(!EmptyChamps_1);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ouvrir une pop-up
+                //openDialog();
+
+                //Ouvrir une activité
+
+                ListAeroports.add(new InfoAeroport(mChampsInput_1.getText().toString()));
+                if(!EmptyChamps_2)
+                    ListAeroports.add(new InfoAeroport(mChampsInput_2.getText().toString()));
+                if(!EmptyChamps_3)
+                    ListAeroports.add(new InfoAeroport(mChampsInput_3.getText().toString()));
+                if(!EmptyChamps_4)
+                    ListAeroports.add(new InfoAeroport(mChampsInput_4.getText().toString()));
+
+              /*
+
+                Intent responseActivity = new Intent(MainActivity.this, ResponseActivity.class);
+                responseActivity.putExtra("name", Name);
+                responseActivity.putExtra("last_name", LName);
+
+                startActivity(responseActivity);*/
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/ensim/snowtam_app/model/InfoAeroport.java
+++ app/src/main/java/com/ensim/snowtam_app/model/InfoAeroport.java
@@ -7,4 +7,39 @@ public class InfoAeroport {
     private String mCoordLat;
     private String mCoordLon;
 
+    public InfoAeroport(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmType() {
+        return mType;
+    }
+
+    public void setmType(String mType) {
+        this.mType = mType;
+    }
+
+    public String getmCoordLat() {
+        return mCoordLat;
+    }
+
+    public void setmCoordLat(String mCoordLat) {
+        this.mCoordLat = mCoordLat;
+    }
+
+    public String getmCoordLon() {
+        return mCoordLon;
+    }
+
+    public void setmCoordLon(String mCoordLon) {
+        this.mCoordLon = mCoordLon;
+    }
 }
